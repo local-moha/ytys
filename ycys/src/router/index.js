676323b27eda56ceb4a496cbd86626683090ab22
莫哈 <local_moha@163.com>
2020-04-30 22:37:12 +0800

    4.30

--- ycys/src/router/index.js
+++ ycys/src/router/index.js
@@ -1,169 +1,169 @@
-
+import Login from '../view/Login'
  export default [
     {
         path:'/login',
-        component:'Login'
+        component:Login
     },
     {
         path:'/distribution',
-        component:'Distribution',
+        component:Distribution
     },
     {
         path:'/error',
-        component:'Error'
+        component:Error
     },
     {
         path:'/',
-        component:'Index',
+        component:Index,
         childrens:[
             {
                 path:'/meisu/',
-                component:'Home'
+                component:Home
             },
             {
                 path:'/superhotel_home.html',
-                component:'Superhotel_home'
+                component:Superhotel_home
             },
             {
                 path:'/superhotel_home.html',
-                component:'Server'
+                component:Server
             },
             {
                 path:'/person.html',
-                component:'Person',
+                component:Person,
                 isAuthorization:true
             },
         ]
     },
     {
         path:'/meisu',
-        component:'Meisu',
+        component:Meisu,
         childrens:[
             {
                 path:'/meisu/super_theme.html',
-                component:'Super_theme'
+                component:Super_theme
             },
             {
                 path:'/meisu/select_location',
-                component:'Select_location'
+                component:Select_location
             },
             {
                 path:'/meisu/search',
-                component:'Search'
+                component:Search
             },
             {
                 path:'/meisu/getCoupon.html',
-                component:'GetCoupon'
+                component:GetCoupon
             },
             {
                 path:'/meisu/stock_list.html',
-                component:'Stock_list'
+                component:Stock_list
             },
             {
                 path:'/meisu/super_list.html',
-                component:'Super_list'
+                component:Super_list
             },
             {
                 path:'/meisu/cutprice.html',
-                component:'Cutprice'
+                component:Cutprice
             },
             {
                 path:'/meisu/theme.html',
-                component:'Theme'
+                component:Theme
             },
             {
                 path:'/meisu/lowprice.html',
-                component:'Lowprice'
+                component:Lowprice
             },
             {
                 path:'/meisu/super_detail.html',
-                component:'Super_detail'
+                component:Super_detail
             },
             {
                 path:'/meisu/mno_list.html',
-                component:'Mno_list.html'
+                component:Mno_list.html
             },
             {
                 path:'/meisu/Mno_detail.html',
-                component:'Theme'
+                component:Theme
             },
             {
                 path:'/meisu/superhotel_day.html',
-                component:'Superhotel_day'
+                component:Superhotel_day
             },
             {
                 path:'/meisu/superhotel_detail.html',
-                component:'Superhotel_detail'
+                component:Superhotel_detail
             },
             {
                 path:'/meisu/order_list.html',
-                component:'Order_list'
+                component:Order_list
             },
             {
                 path:'/meisu/cutprice_orderlist.html',
-                component:'Cutprice_orderlist'
+                component:Cutprice_orderlist
             },
             {
                 path:'/meisu/lowprice.html',
-                component:'Lowprice'
+                component:Lowprice
             },
             {
                 path:'/meisu/super_detail.html',
-                component:'Super_detail'
+                component:Super_detail
             },
         
             {
                 path:'/meisu/mno_list.html',
-                component:'Mno_list'
+                component:Mno_list
             },
             {
                 path:'/meisu/mno_detail.html',
-                component:'Mno_detail'
+                component:Mno_detail
             },
             {
                 path:'/meisu/superhotel_day.html',
-                component:'Superhotel_day'
+                component:Superhotel_day
             },
             
             {
                 path:'/meisu/superhotel_detail.html',
-                component:'Superhotel_detail'
+                component:Superhotel_detail
             },
             {
                 path:'/meisu/order_list.html',
-                component:'Order_list'
+                component:Order_list
             },
             {
                 path:'/meisu/cutprice_orderlist.html',
-                component:'Cutprice_orderlist'
+                component:Cutprice_orderlist
             },
             {
                 path:'/meisu/my_collect.html',
-                component:'My_collect'
+                component:My_collect
             },
             {
                 path:'/meisu/my_coupon.html',
-                component:'My_coupon'
+                component:My_coupon
             },
             {
                 path:'/meisu/exchangeCoupon.html',
-                component:'ExchangeCoupon'
+                component:ExchangeCoupon
             },
             {
                 path:'/meisu/setting.html',
-                component:'Setting'
+                component:Setting
             },
             {
                 path:'/meisu/super_order.html',
-                component:'Super_order'
+                component:Super_order
             },
             {
                 path:'/meisu/super_ordersubmit.html',
-                component:'Super_ordersubmit'
+                component:Super_ordersubmit
             },
             {
                 path:'/meisu/mno_order.html',
-                component:'Mno_order'
+                component:Mno_order
             }
         ]
     }
